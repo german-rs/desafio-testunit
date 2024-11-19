4fc9477eb0b96ae230e03fdb7d204c70e95b873a
german-rs <ing.germanriveros@gmail.com>
2024-11-19 20:09:12 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <header>
+    <div>
+      <nav>
+        <ul>
+          <li><RouterLink to="/about">About</RouterLink></li>
+          <li><RouterLink to="/contact">Contact</RouterLink></li>
+        </ul> 
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<style scoped>
+ul{
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: start;
+  gap: 1em;
+  padding: 1em 0;
+}
+
+li{
+  list-style: none;
+  margin: 0;
+}
+
+li a{
+  text-decoration: none;
+}
+</style>
